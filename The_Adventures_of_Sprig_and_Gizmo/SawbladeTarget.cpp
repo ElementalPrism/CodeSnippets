7722cf890484e91ee730963be853255cc6848893
ElementalPrism <114480418+ElementalPrism@users.noreply.github.com>
2024-04-28 22:28:59 +0100

    Create SawbladeTarget.cpp

---
+++ The_Adventures_of_Sprig_and_Gizmo/SawbladeTarget.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BetterGameProject/Puzzles/SawbladeTarget.h"
+
+// Sets default values
+ASawbladeTarget::ASawbladeTarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASawbladeTarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASawbladeTarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
